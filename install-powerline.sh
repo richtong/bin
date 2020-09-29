161516b2504eaff774a488711a433d78996f3c76
Rich <rich@tongfamily.com>
2020-09-29 10:44:12 -0700

    adding shfmt pre-commit

--- install-powerline.sh
+++ install-powerline.sh
@@ -15,31 +15,30 @@ SCRIPT_DIR=${SCRIPT_DIR:=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
 trap 'exit $?' ERR
 OPTIND=1
 export FLAGS="${FLAGS:-""}"
-while getopts "hdv" opt
-do
-    case "$opt" in
-        h)
-            cat <<-EOF
-Installs Powerline
-    usage: $SCRIPTNAME [ flags ]
-    flags: -d debug, -v verbose, -h help"
-EOF
-            exit 0
-            ;;
-        d)
-            export DEBUGGING=true
-            ;;
-        v)
-            export VERBOSE=true
-            # add the -v which works for many commands
-            export FLAGS+=" -v "
-            ;;
-        *)
-            echo "not flag -$opt"
-            ;;
-    esac
+while getopts "hdv" opt; do
+	case "$opt" in
+	h)
+		cat <<-EOF
+			Installs Powerline
+			    usage: $SCRIPTNAME [ flags ]
+			    flags: -d debug, -v verbose, -h help"
+		EOF
+		exit 0
+		;;
+	d)
+		export DEBUGGING=true
+		;;
+	v)
+		export VERBOSE=true
+		# add the -v which works for many commands
+		export FLAGS+=" -v "
+		;;
+	*)
+		echo "not flag -$opt"
+		;;
+	esac
 done
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 # shellcheck source=./include.sh
 if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
 
@@ -51,40 +50,36 @@ location="$(pip show powerline-status | grep Location | awk '{print $2}')"/power
 log_verbose "powerline completion script at $location"
 
 powerline="$location/bindings/bash/powerline.sh"
-if [[ ! -e $powerline ]]
-then
-    log_error 2 "cannot fine powerline at $powerline"
+if [[ ! -e $powerline ]]; then
+	log_error 2 "cannot fine powerline at $powerline"
 fi
 
-if ! config_mark "$(config_profile_shell)"
-then
-    config_add "$(config_profile_shell)" <<-EOF
-[[ -r $powerline ]] && powerline-daemon -q && \
-    export POWERLINE_BASH_CONTINUATION=1 && \
-    export POWERLINE_BASH_SELECT=1 && \
-    source "$powerline" || true
-EOF
+if ! config_mark "$(config_profile_shell)"; then
+	config_add "$(config_profile_shell)" <<-EOF
+		[[ -r $powerline ]] && powerline-daemon -q && \
+		    export POWERLINE_BASH_CONTINUATION=1 && \
+		    export POWERLINE_BASH_SELECT=1 && \
+		    source "$powerline" || true
+ EOF
 fi
 
 config="$location/config_files"
 PROFILE="${PROFILE:-"$HOME/.config/powerline"}"
-if [[ ! -e $PROFILE/config.json ]]
-then
-    log_verbose "copying from $config to $PROFILE"
-    cp -r "$config/"* "$PROFILE"
+if [[ ! -e $PROFILE/config.json ]]; then
+ log_verbose "copying from $config to $PROFILE"
+ cp -r "$config/"* "$PROFILE"
 fi
 
 log_verbose "installing powerline fonts add to config.json"
 cask_install font-fira-mono-for-powerline
 
 VIM_PROFILE="${VIM_PROFILE:"$HOME/.vimrc"}"
-if ! config_mark "$VIM_PROFILE"
-then
-    log_verbose "adding to $VIM_PROFILE"
-    config_add "$VIM_PROFILE" "'" <<-EOF
-set rtp+="$location/bindings/vim"
-set laststatus=2
-EOF
+if ! config_mark "$VIM_PROFILE"; then
+ log_verbose "adding to $VIM_PROFILE"
+ config_add "$VIM_PROFILE" "'" <<-EOF
+		set rtp+="$location/bindings/vim"
+		set laststatus=2
+ EOF
 fi
 
 # https://github.com/gravyboat/powerline-config
