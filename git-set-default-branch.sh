02d3fe50cd039dfd9f76076858f77d5ab9b8a03b
Rich Tong <rich@tongfamily.com>
2021-02-11 22:38:15 -0800

    fix to git-set-default-branch.sh and added git-sbumodule-checkout-default.sh

--- git-set-default-branch.sh
+++ git-set-default-branch.sh
@@ -18,8 +18,6 @@ OPTIND=1
 LEAVE="${LEAVE:-false}"
 DRY_RUN_FLAG="${DRY_RUN_FLAG:-false}"
 DRY_RUN="${DRY_RUN:-"echo"}"
-OLD_DEFAULT="${OLD_DEFAULT:-master}"
-NEW_DEFAULT="${NEW_DEFAULT:-main}"
 REMOTE="${REMOTE:-origin}"
 export FLAGS="${FLAGS:-""}"
 while getopts "hdvlf:t:r:n" opt; do
@@ -74,16 +72,38 @@ if ! $DRY_RUN_FLAG; then
 	DRY_RUN=""
 fi
 
-log_warning "As of Feb 2021 this does not seem to work go to github.com"
+log_warning "As of Feb 2021 this does not seem to work go to github.com it is"
+log_warning "best to use the gui see https://github.com/github/renaming"
 log_warning "settings/branches and change the branch which will create the new one"
 
+# https://github.com/github/renaming
 # https://stevenmortimer.com/5-steps-to-change-github-default-branch-from-master-to-main/
 # https://dev.to/softprops/digitally-unmastered-the-github-cli-edition-1cc4
-log_verbose "changing default with gh api -XPATCH repos/:owner/:repo -f default_branch=$NEW_DEFAULT"
+# https://www.hanselman.com/blog/easily-rename-your-git-default-branch-from-master-to-main
+# finding default branch on github
+# https://dev.to/bowmanjd/get-github-default-branch-from-the-command-line-powershell-or-bash-zsh-37m9
+# shellcheck disable=SC2016
+# https://docs.github.com/en/github/administering-a-repository/renaming-a-branch
+
+# https://stackoverflow.com/questions/28666357/git-how-to-get-default-branch
+# git symbolic-ref refs/remotes/origin/HEAD does not work, it always returns
+# only the original master, you have to actually run an api call to get it. :w
+
+# https://stackoverflow.com/questions/28666357/git-how-to-get-default-branch
+log_verbose "Determine the default branch on github"
+OLD_DEFAULT=$(git_default_branch)
+log_verbose "current default branch is $OLD_DEFAULT"
+
+if [[ $OLD_DEFAULT == "$NEW_DEFAULT" ]]; then
+	log_exit "default is already $NEW_DEFAULT"
+fi
+
 # shellcheck disable=SC2016
 CMDS=(
-	'git symbolic-ref refs/remotes/origin/HEAD "refs/remotes/origin/$NEW_DEFAULT"'
 	'gh api -XPATCH "repos/:owner/:repo" -f default_branch="$NEW_DEFAULT" >/dev/null'
+	'git branch -m "$OLD_DEFAULT" "$NEW_DEFAULT"'
+	'git fetch "$ORIGIN"'
+	'git branch -u "$ORIGIN/$NEW_DEFAULT" "$NEW_DEFAULT"'
 )
 
 for cmd in "${CMDS[@]}"; do
---
+++ git-submodule-checkout-default.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+# the check we need to do this for DRY_RUN since we don't want to glob
+##
+##
+## sets all submodules to their default branch
+##
+##@author Rich Tong
+##@returns 0 on success
+#
+# To enable compatibility with bashdb instead of set -e
+# https://marketplace.visualstudio.com/items?itemName=rogalmic.bash-debug
+# use the trap on ERR
+set -u && SCRIPTNAME="$(basename "${BASH_SOURCE[0]}")"
+SCRIPT_DIR=${SCRIPT_DIR:=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
+# this replace set -e by running exit on any error use for bashdb
+trap 'exit $?' ERR
+OPTIND=1
+ORIGIN_REMOTE="${ORIGIN_REMOTE:-origin}"
+ORIGIN_DEFAULT="${ORIGIN_DEFAULT:-main}"
+FORCE_FLAG="${FORCE_FLAG:-false}"
+DRY_RUN="${DRY_RUN:-""}"
+DRY_RUN_FLAG="${DRY_RUN_FLAG:-false}"
+DEST_REPO_PATH="${DEST_REPO_PATH:-"$PWD"}"
+export FLAGS="${FLAGS:-""}"
+while getopts "hdvfng:p:m:l:" opt; do
+	case "$opt" in
+	h)
+		cat <<-EOF
+			Gets the organization ready for a commit to main by
+			Merge upstream changes from $UPSTREAM_ORG/$UPSTREAM_DEFAULT to origin/$ORIGIN_DEFAULT
+			Rebase current branches to origin/$MAIN and push the changes to origin/$MAIN
+			    usage: $SCRIPTNAME [ flags ]
+			    flags: -d debug, -v verbose, -h help"
+					   -f force pushs (default: $FORCE_FLAG)
+					   -n dry run (default: $DRY_RUN_FLAG)
+					   -l Origin remote name (default: $ORIGIN_REMOTE)
+					   -m Origin branch that is the default (default: $ORIGIN_DEFAULT)
+			           -g Git repo Url extension (default: $GITHUB_URL)
+			           -p The path to the repo being created (default: $DEST_REPO_PATH)
+			    Note that repos cannot have white space in their names
+		EOF
+		exit 0
+		;;
+	d)
+		export DEBUGGING=true
+		;;
+	v)
+		export VERBOSE=true
+		# add the -v which works for many commands
+		export FLAGS+=" -v "
+		;;
+	f)
+		FORCE_FLAG=true
+		;;
+	n)
+		DRY_RUN_FLAG=true
+		;;
+	l)
+		ORIGIN_REMOTE="$OPTARG"
+		;;
+	m)
+		ORIGIN_DEFAULT="$OPTARG"
+		;;
+	p)
+		DEST_REPO_PATH="$OPTARG"
+		;;
+	*)
+		echo "not flag -$opt"
+		;;
+	esac
+done
+shift $((OPTIND - 1))
+# shellcheck source=./include.sh
+if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
+source_lib lib-git.sh lib-util.sh
+
+DRY_RUN=""
+if $DRY_RUN_FLAG; then
+	DRY_RUN="echo"
+fi
+log_verbose "DRY_RUN is $DRY_RUN"
+
+FORCE=""
+if $FORCE_FLAG; then
+	# shellcheck disable=SC2034
+	# only appear unused but is used in the eval
+	FORCE="-f"
+fi
+
+if ! pushd "$DEST_REPO_PATH" >/dev/null; then
+	log_error 1 "no $DEST_REPO_PATH"
+fi
+log_verbose "in $PWD"
+
+if ! git_repo; then
+	log_error 2 "$DEST_REPO_PATH is not a git repo"
+fi
+
+log_verbose "assume the remote origin is the default remote $ORIGIN_REMOTE"
+# shellcheck disable=SC2016
+git submodule foreach \
+	'git remote set-head origin --auto && git checkout $(basename $(git rev-parse --abbrev-ref $ORIGIN_REMOTE/HEAD)) && git pull --rebase'
