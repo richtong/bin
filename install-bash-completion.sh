a2492176b17c2ef94f71566e22c6d009d05cac91
Rich Tong <rich@tongfamily.com>
2021-02-15 15:59:01 -0800

    install-bash-completion typos

--- install-bash-completion.sh
+++ install-bash-completion.sh
@@ -64,7 +64,7 @@ if ! config_mark "$(config_profile_shell)"; then
 			if [[ -r "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" ]]; then
 				source "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh"
 			else
-				for COMPLETION in ":$HOMEBREW_PREFIX/etc/bash_completion.d"/*; do
+				for COMPLETION in "$HOMEBREW_PREFIX/etc/bash_completion.d/"*; do
 					[[ -r "$COMPLETION" ]] && source "$COMPLETION"
 				done
 			fi
--- install-gcloud.sh
+++ install-gcloud.sh
@@ -17,66 +17,61 @@ PACKAGES="${PACKAGES:-" beta "}"
 trap 'exit $?' ERR
 OPTIND=1
 export FLAGS="${FLAGS:-""}"
-while getopts "hdv" opt
-do
-    case "$opt" in
-        h)
-            cat <<-EOF
-Installs Google Cloud and components
-    usage: $SCRIPTNAME [ flags ] additional_components (default: $PACKAGES)
-    flags: -d debug, -v verbose, -h help"
-EOF
-            exit 0
-            ;;
-        d)
-            export DEBUGGING=true
-            ;;
-        v)
-            export VERBOSE=true
-            # add the -v which works for many commands
-            export FLAGS+=" -v "
-            ;;
-        *)
-            echo "no -$opt" >&2
-    esac
+while getopts "hdv" opt; do
+	case "$opt" in
+	h)
+		cat <<-EOF
+			Installs Google Cloud and components
+			    usage: $SCRIPTNAME [ flags ] additional_components (default: $PACKAGES)
+			    flags: -d debug, -v verbose, -h help"
+		EOF
+		exit 0
+		;;
+	d)
+		export DEBUGGING=true
+		;;
+	v)
+		export VERBOSE=true
+		# add the -v which works for many commands
+		export FLAGS+=" -v "
+		;;
+	*)
+		echo "no -$opt" >&2
+		;;
+	esac
 done
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 # shellcheck source=./include.sh
 if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
 
 source_lib lib-install.sh lib-util.sh lib-config.sh
 
-
-if ! in_os mac
-then
-    log_exit "Not tested beyond MacOS"
+if ! in_os mac; then
+	log_exit "Not tested beyond MacOS"
 fi
 log_verbose install google cloud sdk
 cask_install google-cloud-sdk
 
 log_verbose "checking for gcloud in $(config_profile)"
-if ! config_mark
-then
-    log_verbose "installing into $(config_profile)"
-    config_add <<-EOF
-    source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
-    source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
-EOF
-    log_verbose "now source the changes to $(config_profile)"
-    source_profile
-    hash -r
+if ! config_mark; then
+	log_verbose "installing into $(config_profile)"
+	config_add <<-EOF
+		source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
+		source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
+	EOF
+	log_verbose "now source the changes to $(config_profile)"
+	source_profile
+	hash -r
 fi
 
 log_verbose install additional packages "$PACKAGES" "$@"
-
 
 # all packages cannot contain spaces
 # shellcheck disable=SC2086
 gcloud components install --quiet $PACKAGES "$@"
 
 # https://stackoverflow.com/questions/42379685/can-i-automate-google-cloud-sdk-gcloud-init-interactive-command
-if [[ $(gcloud config configurations list | wc -l ) -lt 2 ]]
-then
-    log_verbose no configuration exists so init
-    gcloud init
+if [[ $(gcloud config configurations list | wc -l) -lt 2 ]]; then
+	log_verbose no configuration exists so init
+	gcloud init
 fi
