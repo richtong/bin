aed48b8231f58493da3f37e77e1a793ae7109a22
Rich Tong <1782087+richtong@users.noreply.github.com>
2021-03-20 14:40:00 -0700

    install-lint adds gem mdl and install-ruby finds right version

--- install-lint.sh
+++ install-lint.sh
@@ -15,35 +15,34 @@ trap 'exit $?' ERR
 OPTIND=1
 VERSION="${VERSION:-7}"
 export FLAGS="${FLAGS:-""}"
-while getopts "hdvr:" opt
-do
-    case "$opt" in
-        h)
-            cat <<-EOF
-Installs Linters
-    usage: $SCRIPTNAME [ flags ]
-    flags: -d debug, -v verbose, -h help"
-           -r version number (default: $VERSION)
-EOF
-            exit 0
-            ;;
-        d)
-            export DEBUGGING=true
-            ;;
-        v)
-            export VERBOSE=true
-            # add the -v which works for many commands
-            export FLAGS+=" -v "
-            ;;
-        r)
-            VERSION="$OPTARG"
-            ;;
-        *)
-            echo "not flag -$opt"
-            ;;
-    esac
+while getopts "hdvr:" opt; do
+	case "$opt" in
+	h)
+		cat <<-EOF
+			Installs Linters
+			    usage: $SCRIPTNAME [ flags ]
+			    flags: -d debug, -v verbose, -h help"
+			           -r version number (default: $VERSION)
+		EOF
+		exit 0
+		;;
+	d)
+		export DEBUGGING=true
+		;;
+	v)
+		export VERBOSE=true
+		# add the -v which works for many commands
+		export FLAGS+=" -v "
+		;;
+	r)
+		VERSION="$OPTARG"
+		;;
+	*)
+		echo "not flag -$opt"
+		;;
+	esac
 done
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 # shellcheck source=./include.sh
 if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
 source_lib lib-git.sh lib-mac.sh lib-install.sh lib-util.sh lib-config.sh
@@ -51,33 +50,42 @@ source_lib lib-git.sh lib-mac.sh lib-install.sh lib-util.sh lib-config.sh
 "$SCRIPT_DIR/install-node.sh"
 log_verbose install linters
 NODE_PACKAGES+=(
-    eslint
-    babel-eslint
-    eslint-plugin-react
-    eslint-plugin-import
-    eslint-plugin-jsx-a11y
-    eslint-plugin-flowtype
-    jslint
-    jsonlint
-    js-yaml
-    markdown-cli
+	eslint
+	babel-eslint
+	eslint-plugin-react
+	eslint-plugin-import
+	eslint-plugin-jsx-a11y
+	eslint-plugin-flowtype
+	jslint
+	jsonlint
+	js-yaml
+	markdown-cli
+	markdownlint
 )
 
+log_verbose "install ${NODE_PACKAGES[*]}"
 npm_install -g "${NODE_PACKAGES[@]}"
 
-PIP_PACKAGES+=(
-    jedi
-    vim-vint
-    beautysh
+PYTHON_PACKAGES+=(
+	jedi
+	vim-vint
+	beautysh
 )
 
-pip_install "${PIP_PACKAGES[@]}"
+log_verbose "install ${PYTHON_PACKAGES[*]}"
+pip_install "${PYTHON_PACKAGES[@]}"
+
+RUBY_PACKAGES+=(
+	mdl
+)
+log_verbose "install ${RUBY_PACKAGES[*]}"
+gem_install "${RUBY_PACKAGES[@]}"
 
 BREW_PACKAGES+=(
-    yapf
-    shellcheck
-    shfmt
-    yamllint
+	yapf
+	shellcheck
+	shfmt
+	yamllint
 )
 
 brew_install "${BREW_PACKAGES[@]}"
@@ -93,13 +101,11 @@ log_verbose need flake8 for python linting
 # python https://flake8.pycqa.org/en/latest/
 # this merges server checkers
 # not robust
-if [[ $OSTYPE =~ darwin ]]
-then
-    brew_install flake8
+if [[ $OSTYPE =~ darwin ]]; then
+	brew_install flake8
 else
-    pip_install --user --upgrade flake8
+	pip_install --user --upgrade flake8
 fi
-
 
 # https://github.com/yannickcr/eslint-plugin-react
 # https://github.com/facebookincubator/create-react-app/blob/master/template/README.md#displaying-lint-output-in-the-editor
@@ -110,184 +116,183 @@ fi
 # We do not want flags to exist if they are null
 log_verbose "checking $HOME/.eslintrc.js"
 # shellcheck disable=SC2086
-if ! config_mark "$HOME/.eslintrc.js" "//"
-then
-    log_verbose "Addint to $HOME/.eslintrc.js"
-    config_add "$HOME/.eslintrc.js" <<-'EOF'
-{
-  root: true,
-  parser: 'babel-eslint',
-  // import plugin is temporarily disabled, scroll below to see why
-  plugins: ['import', 'flowtype', 'jsx-a11y', 'react'],
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    jest: true,
-    node: true
-  },
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-      generators: true,
-      experimentalObjectRestSpread: true
-    }
-  },
-  settings: {
-    'import/ignore': [
-      'node_modules',
-      '\\.(json|css|jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm)$',
-    ],
-    'import/extensions': ['.js'],
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.json']
-      }
-    }
-  },
-  rules: {
-    // http://eslint.org/docs/rules/
-    'array-callback-return': 'warn',
-    'default-case': ['warn', { commentPattern: '^no default$' }],
-    'dot-location': ['warn', 'property'],
-    eqeqeq: ['warn', 'allow-null'],
-    'guard-for-in': 'warn',
-    'new-parens': 'warn',
-    'no-array-constructor': 'warn',
-    'no-caller': 'warn',
-    'no-cond-assign': ['warn', 'always'],
-    'no-const-assign': 'warn',
-    'no-control-regex': 'warn',
-    'no-delete-var': 'warn',
-    'no-dupe-args': 'warn',
-    'no-dupe-class-members': 'warn',
-    'no-dupe-keys': 'warn',
-    'no-duplicate-case': 'warn',
-    'no-empty-character-class': 'warn',
-    'no-empty-pattern': 'warn',
-    'no-eval': 'warn',
-    'no-ex-assign': 'warn',
-    'no-extend-native': 'warn',
-    'no-extra-bind': 'warn',
-    'no-extra-label': 'warn',
-    'no-fallthrough': 'warn',
-    'no-func-assign': 'warn',
-    'no-implied-eval': 'warn',
-    'no-invalid-regexp': 'warn',
-    'no-iterator': 'warn',
-    'no-label-var': 'warn',
-    'no-labels': ['warn', { allowLoop: false, allowSwitch: false }],
-    'no-lone-blocks': 'warn',
-    'no-loop-func': 'warn',
-    'no-mixed-operators': ['warn', {
-      groups: [
-        ['&', '|', '^', '~', '<<', '>>', '>>>'],
-        ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
-        ['&&', '||'],
-        ['in', 'instanceof']
-      ],
-      allowSamePrecedence: false
-    }],
-    'no-multi-str': 'warn',
-    'no-native-reassign': 'warn',
-    'no-negated-in-lhs': 'warn',
-    'no-new-func': 'warn',
-    'no-new-object': 'warn',
-    'no-new-symbol': 'warn',
-    'no-new-wrappers': 'warn',
-    'no-obj-calls': 'warn',
-    'no-octal': 'warn',
-    'no-octal-escape': 'warn',
-    'no-redeclare': 'warn',
-    'no-regex-spaces': 'warn',
-    'no-restricted-syntax': [
-      'warn',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-    'no-script-url': 'warn',
-    'no-self-assign': 'warn',
-    'no-self-compare': 'warn',
-    'no-sequences': 'warn',
-    'no-shadow-restricted-names': 'warn',
-    'no-sparse-arrays': 'warn',
-    'no-template-curly-in-string': 'warn',
-    'no-this-before-super': 'warn',
-    'no-throw-literal': 'warn',
-    'no-undef': 'warn',
-    'no-unexpected-multiline': 'warn',
-    'no-unreachable': 'warn',
-    'no-unused-expressions': 'warn',
-    'no-unused-labels': 'warn',
-    'no-unused-vars': ['warn', { vars: 'local', args: 'none' }],
-    'no-use-before-define': ['warn', 'nofunc'],
-    'no-useless-computed-key': 'warn',
-    'no-useless-concat': 'warn',
-    'no-useless-constructor': 'warn',
-    'no-useless-escape': 'warn',
-    'no-useless-rename': ['warn', {
-      ignoreDestructuring: false,
-      ignoreImport: false,
-      ignoreExport: false,
-    }],
-    'no-with': 'warn',
-    'no-whitespace-before-property': 'warn',
-    'operator-assignment': ['warn', 'always'],
-    radix: 'warn',
-    'require-yield': 'warn',
-    'rest-spread-spacing': ['warn', 'never'],
-    strict: ['warn', 'never'],
-    'unicode-bom': ['warn', 'never'],
-    'use-isnan': 'warn',
-    'valid-typeof': 'warn',
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/
-    // ok for vim
-    // TODO: import rules are temporarily disabled because they don't play well
-    // with how eslint-loader only checks the file you change. So if module A
-    // imports module B, and B is missing a default export, the linter will
-    // record this as an issue in module A. Now if you fix module B, the linter
-    // will not be aware that it needs to re-lint A as well, so the error
-    // will stay until the next restart, which is really confusing.
-    // This is probably fixable with a patch to eslint-loader.
-    // When file A is saved, we want to invalidate all files that import it
-    // *and* that currently have lint errors. This should fix the problem.
-    'import/default': 'warn',
-    'import/export': 'warn',
-    'import/named': 'warn',
-    'import/namespace': 'warn',
-    'import/no-amd': 'warn',
-    'import/no-duplicates': 'warn',
-    'import/no-extraneous-dependencies': 'warn',
-    'import/no-named-as-default': 'warn',
-    'import/no-named-as-default-member': 'warn',
-    'import/no-unresolved': ['warn', { commonjs: true }],
-    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
-    'react/jsx-equals-spacing': ['warn', 'never'],
-    'react/jsx-no-duplicate-props': ['warn', { ignoreCase: true }],
-    'react/jsx-no-undef': 'warn',
-    'react/jsx-pascal-case': ['warn', {
-      allowAllCaps: true,
-      ignore: [],
-    }],
-    'react/jsx-uses-react': 'warn',
-    'react/jsx-uses-vars': 'warn',
-    'react/no-deprecated': 'warn',
-    'react/no-direct-mutation-state': 'warn',
-    'react/no-is-mounted': 'warn',
-    'react/react-in-jsx-scope': 'warn',
-    'react/require-render-return': 'warn',
-    // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
-    'jsx-a11y/aria-role': 'warn',
-    'jsx-a11y/img-has-alt': 'warn',
-    'jsx-a11y/img-redundant-alt': 'warn',
-    'jsx-a11y/no-access-key': 'warn',
-    // https://github.com/gajus/eslint-plugin-flowtype
-    'flowtype/define-flow-type': 'warn',
-    'flowtype/require-valid-file-annotation': 'warn',
-    'flowtype/use-flow-type': 'warn'
-  }
-};
-EOF
+if ! config_mark "$HOME/.eslintrc.js" "//"; then
+	log_verbose "Addint to $HOME/.eslintrc.js"
+	config_add "$HOME/.eslintrc.js" <<-'EOF'
+		{
+		  root: true,
+		  parser: 'babel-eslint',
+		  // import plugin is temporarily disabled, scroll below to see why
+		  plugins: ['import', 'flowtype', 'jsx-a11y', 'react'],
+		  env: {
+		    browser: true,
+		    commonjs: true,
+		    es6: true,
+		    jest: true,
+		    node: true
+		  },
+		  parserOptions: {
+		    ecmaVersion: 6,
+		    sourceType: 'module',
+		    ecmaFeatures: {
+		      jsx: true,
+		      generators: true,
+		      experimentalObjectRestSpread: true
+		    }
+		  },
+		  settings: {
+		    'import/ignore': [
+		      'node_modules',
+		      '\\.(json|css|jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm)$',
+		    ],
+		    'import/extensions': ['.js'],
+		    'import/resolver': {
+		      node: {
+		        extensions: ['.js', '.json']
+		      }
+		    }
+		  },
+		  rules: {
+		    // http://eslint.org/docs/rules/
+		    'array-callback-return': 'warn',
+		    'default-case': ['warn', { commentPattern: '^no default$' }],
+		    'dot-location': ['warn', 'property'],
+		    eqeqeq: ['warn', 'allow-null'],
+		    'guard-for-in': 'warn',
+		    'new-parens': 'warn',
+		    'no-array-constructor': 'warn',
+		    'no-caller': 'warn',
+		    'no-cond-assign': ['warn', 'always'],
+		    'no-const-assign': 'warn',
+		    'no-control-regex': 'warn',
+		    'no-delete-var': 'warn',
+		    'no-dupe-args': 'warn',
+		    'no-dupe-class-members': 'warn',
+		    'no-dupe-keys': 'warn',
+		    'no-duplicate-case': 'warn',
+		    'no-empty-character-class': 'warn',
+		    'no-empty-pattern': 'warn',
+		    'no-eval': 'warn',
+		    'no-ex-assign': 'warn',
+		    'no-extend-native': 'warn',
+		    'no-extra-bind': 'warn',
+		    'no-extra-label': 'warn',
+		    'no-fallthrough': 'warn',
+		    'no-func-assign': 'warn',
+		    'no-implied-eval': 'warn',
+		    'no-invalid-regexp': 'warn',
+		    'no-iterator': 'warn',
+		    'no-label-var': 'warn',
+		    'no-labels': ['warn', { allowLoop: false, allowSwitch: false }],
+		    'no-lone-blocks': 'warn',
+		    'no-loop-func': 'warn',
+		    'no-mixed-operators': ['warn', {
+		      groups: [
+		        ['&', '|', '^', '~', '<<', '>>', '>>>'],
+		        ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+		        ['&&', '||'],
+		        ['in', 'instanceof']
+		      ],
+		      allowSamePrecedence: false
+		    }],
+		    'no-multi-str': 'warn',
+		    'no-native-reassign': 'warn',
+		    'no-negated-in-lhs': 'warn',
+		    'no-new-func': 'warn',
+		    'no-new-object': 'warn',
+		    'no-new-symbol': 'warn',
+		    'no-new-wrappers': 'warn',
+		    'no-obj-calls': 'warn',
+		    'no-octal': 'warn',
+		    'no-octal-escape': 'warn',
+		    'no-redeclare': 'warn',
+		    'no-regex-spaces': 'warn',
+		    'no-restricted-syntax': [
+		      'warn',
+		      'LabeledStatement',
+		      'WithStatement',
+		    ],
+		    'no-script-url': 'warn',
+		    'no-self-assign': 'warn',
+		    'no-self-compare': 'warn',
+		    'no-sequences': 'warn',
+		    'no-shadow-restricted-names': 'warn',
+		    'no-sparse-arrays': 'warn',
+		    'no-template-curly-in-string': 'warn',
+		    'no-this-before-super': 'warn',
+		    'no-throw-literal': 'warn',
+		    'no-undef': 'warn',
+		    'no-unexpected-multiline': 'warn',
+		    'no-unreachable': 'warn',
+		    'no-unused-expressions': 'warn',
+		    'no-unused-labels': 'warn',
+		    'no-unused-vars': ['warn', { vars: 'local', args: 'none' }],
+		    'no-use-before-define': ['warn', 'nofunc'],
+		    'no-useless-computed-key': 'warn',
+		    'no-useless-concat': 'warn',
+		    'no-useless-constructor': 'warn',
+		    'no-useless-escape': 'warn',
+		    'no-useless-rename': ['warn', {
+		      ignoreDestructuring: false,
+		      ignoreImport: false,
+		      ignoreExport: false,
+		    }],
+		    'no-with': 'warn',
+		    'no-whitespace-before-property': 'warn',
+		    'operator-assignment': ['warn', 'always'],
+		    radix: 'warn',
+		    'require-yield': 'warn',
+		    'rest-spread-spacing': ['warn', 'never'],
+		    strict: ['warn', 'never'],
+		    'unicode-bom': ['warn', 'never'],
+		    'use-isnan': 'warn',
+		    'valid-typeof': 'warn',
+		    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/
+		    // ok for vim
+		    // TODO: import rules are temporarily disabled because they don't play well
+		    // with how eslint-loader only checks the file you change. So if module A
+		    // imports module B, and B is missing a default export, the linter will
+		    // record this as an issue in module A. Now if you fix module B, the linter
+		    // will not be aware that it needs to re-lint A as well, so the error
+		    // will stay until the next restart, which is really confusing.
+		    // This is probably fixable with a patch to eslint-loader.
+		    // When file A is saved, we want to invalidate all files that import it
+		    // *and* that currently have lint errors. This should fix the problem.
+		    'import/default': 'warn',
+		    'import/export': 'warn',
+		    'import/named': 'warn',
+		    'import/namespace': 'warn',
+		    'import/no-amd': 'warn',
+		    'import/no-duplicates': 'warn',
+		    'import/no-extraneous-dependencies': 'warn',
+		    'import/no-named-as-default': 'warn',
+		    'import/no-named-as-default-member': 'warn',
+		    'import/no-unresolved': ['warn', { commonjs: true }],
+		    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
+		    'react/jsx-equals-spacing': ['warn', 'never'],
+		    'react/jsx-no-duplicate-props': ['warn', { ignoreCase: true }],
+		    'react/jsx-no-undef': 'warn',
+		    'react/jsx-pascal-case': ['warn', {
+		      allowAllCaps: true,
+		      ignore: [],
+		    }],
+		    'react/jsx-uses-react': 'warn',
+		    'react/jsx-uses-vars': 'warn',
+		    'react/no-deprecated': 'warn',
+		    'react/no-direct-mutation-state': 'warn',
+		    'react/no-is-mounted': 'warn',
+		    'react/react-in-jsx-scope': 'warn',
+		    'react/require-render-return': 'warn',
+		    // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
+		    'jsx-a11y/aria-role': 'warn',
+		    'jsx-a11y/img-has-alt': 'warn',
+		    'jsx-a11y/img-redundant-alt': 'warn',
+		    'jsx-a11y/no-access-key': 'warn',
+		    // https://github.com/gajus/eslint-plugin-flowtype
+		    'flowtype/define-flow-type': 'warn',
+		    'flowtype/require-valid-file-annotation': 'warn',
+		    'flowtype/use-flow-type': 'warn'
+		  }
+		};
+	EOF
 fi
--- install-ruby.sh
+++ install-ruby.sh
@@ -12,46 +12,55 @@ set -e && SCRIPTNAME="$(basename "$0")"
 SCRIPT_DIR=${SCRIPT_DIR:-"$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"}
 
 OPTIND=1
-while getopts "hdvr:" opt
-do
-    case "$opt" in
-        h)
-            echo "$SCRIPTNAME: install ruby"
-            echo  "flags: -d debug, -h help, -r ruby version"
-            exit 0
-            ;;
-        d)
-            export DEBUGGING=true
-            ;;
-        v)
-            export VERBOSE=true
-            ;;
-        r)
-            VERSION="$OPTARG"
-            ;;
-        *)
-            echo "no -$opt" >&2
-            ;;
-    esac
+while getopts "hdvr:" opt; do
+	case "$opt" in
+	h)
+		echo "$SCRIPTNAME: install ruby"
+		echo "flags: -d debug, -h help, -r ruby version"
+		exit 0
+		;;
+	d)
+		export DEBUGGING=true
+		;;
+	v)
+		export VERBOSE=true
+		;;
+	r)
+		VERSION="$OPTARG"
+		;;
+	*)
+		echo "no -$opt" >&2
+		;;
+	esac
 done
 # shellcheck source=./include.sh
 if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
-
+log_verbose "about to source"
 source_lib lib-version-compare.sh lib-util.sh lib-config.sh
 
 VERSION=${VERSION:-2.1}
 
 set -u
 
-if in_os mac
-then
-    brew install ruby
-    if ! config_mark
-    then
-        # shellcheck disable=SC2016
-        config_add <<<'export PATH="/usr/local/opt/ruby/bin:$PATH"'
-    fi
-    exit
+if in_os mac; then
+	RUBY_PATH="${RUBY_PATH:-"/usr/local/opt/ruby/bin/"}"
+	RUBY_LIB="${RUBY_LIB:-"/usr/local/lib/ruby/gems"}"
+	log_verbose "installing ruby"
+	brew install ruby
+	log_verbose "raw version is $("$RUBY_PATH/ruby" -v)"
+	version="$("$RUBY_PATH/ruby" -v | cut -d ' ' -f 2)"
+	log_verbose "ruby version $version"
+	version="$(echo "$version" | util_semver)"
+	log_verbose "ruby version after util_semvar $version"
+	if ! config_mark; then
+		log_verbose "installing gem bin $version and ruby"
+		# shellcheck disable=SC2016
+		config_add <<-EOF
+			[[ \$PATH =~ $RUBY_PATH ]] || PATH="$RUBY_PATH:\$PATH"
+			[[ \$PATH =~ $RUBY_LIB/$version/bin ]] || PATH+=":$RUBY_LIB/$version/bin"
+		EOF
+	fi
+	exit
 fi
 
 # For travis using 12.04, need different install
@@ -60,17 +69,17 @@ fi
 ##@param $1 package name
 ##@param $2 ppa repository
 install() {
-    log_verbose "installing $1 from $2"
-    sudo apt-get install -y python-software-properties
-    sudo add-apt-repository -r -y  "$2"
-    sudo add-apt-repository -y "$2"
-    sudo apt-get update
-    sudo apt-get install -y "$1"
+	log_verbose "installing $1 from $2"
+	sudo apt-get install -y python-software-properties
+	sudo add-apt-repository -r -y "$2"
+	sudo add-apt-repository -y "$2"
+	sudo apt-get update
+	sudo apt-get install -y "$1"
 }
 
-if ! command -v ruby || verlt "$(ruby -v | cut -d' ' -f 2)" 1.9.3
-then
-    install "ruby2.1" "ppa:brightbox/ruby-ng"
-    sudo apt-get install -y ruby-switch "ruby$VERSION-dev"
-    sudo ruby-switch --set ruby "$VERSION"
+if ! command -v ruby || verlt "$(ruby -v | cut -d' ' -f 2)" 1.9.3; then
+	install "ruby2.1" "ppa:brightbox/ruby-ng"
+	sudo apt-get install -y ruby-switch "ruby$VERSION-dev"
+	sudo ruby-switch --set ruby "$VERSION"
+
 fi
