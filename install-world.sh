1111441cfa38899561ec57e037bf4e7dacefe769
Rich Tong <1782087+richtong@users.noreply.github.com>
2021-03-21 18:15:27 -0700

    install-world installs cli

--- install-world.sh
+++ install-world.sh
@@ -14,16 +14,23 @@ SCRIPT_DIR=${SCRIPT_DIR:=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
 # this replace set -e by running exit on any error use for bashdb
 trap 'exit $?' ERR
 OPTIND=1
+APP_NAME="${APP_NAME:-COLMAP}"
 RELEASE="${RELEASE:-3.6}"
+APP_URL="${APP_URL:-"https://github.com/colmap/colmap/releases/download/$RELEASE/$APP_NAME-$RELEASE-mac-no-cuda.zip"}"
+APP_BIN="${APP_BIN:-"/Applications/$APP_NAME.app/Contents/MacOS"}"
 export FLAGS="${FLAGS:-""}"
-while getopts "hdvr:" opt; do
+while getopts "hdvfa:r:u:b:" opt; do
 	case "$opt" in
 	h)
 		cat <<-EOF
 			Installs Mapping Software
 			    usage: $SCRIPTNAME [ flags ]
 			    flags: -d debug, -v verbose, -h help"
+					   -f force install (default: $FORCE)
+					   -a application name (default: $APP_NAME)
 			           -r version number (default: $RELEASE)
+					   -u download url (defualt: $APP_URL)
+					   -b binary path (default: $APP_BIN)
 		EOF
 		exit 0
 		;;
@@ -35,8 +42,20 @@ while getopts "hdvr:" opt; do
 		# add the -v which works for many commands
 		export FLAGS+=" -v "
 		;;
+	f)
+		FORCE=true
+		;;
+	a)
+		APP_NAME="$OPTARG"
+		;;
 	r)
 		RELEASE="$OPTARG"
+		;;
+	u)
+		APP_URL="$OPTARG"
+		;;
+	b)
+		APP_BIN="$OPTARG"
 		;;
 	*)
 		echo "not flag -$opt"
@@ -47,14 +66,23 @@ shift $((OPTIND - 1))
 # shellcheck source=./include.sh
 if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
 
-source_lib lib-git.sh lib-mac.sh lib-install.sh lib-util.sh
+source_lib lib-util.sh lib-install.sh lib-config.sh
 
 if in_os mac; then
-	log_verbose "Downloading Colmap"
-	download_url_open \
-		"https://github.com/colmap/colmap/releases/download/$RELEASE/COLMAP-$RELEASE-mac-no-cuda.zip"
-	log_verbose "Move .app to /Applications"
-	mv "$WS_DIR/cache/COLMAP.app" "/Applications"
+	log_verbose "Downloading $APP_NAME"
+	download_url_open "$APP_URL"
+	log_verbose "Move $APP_NAME.app to /Applications"
+	APP_DIR="/Applications/$APP_NAME.app"
+	if [[ -e $APP_DIR ]] && $FORCE; then
+		rm -rf "$APP_DIR"
+	fi
+	mv "$WS_DIR/cache/$APP_NAME.app" "$(dirname "$APP_DIR")"
+	log_verbose "Making CLI available"
+	if ! config_mark; then
+		config_add <<-EOF
+			[[ \$PATH =~ $APP_BIN ]] || PATH+=":$APP_BIN"
+		EOF
+	fi
 	log_verbose "Download sample images from https://colmap.github.io/datasets.html#datasets"
 	log_verbose "Choose Reconstruction/Automatic Reconstruction"
 	log_verbose "And pick workspace and image folders"
