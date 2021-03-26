332637ea301abd9f9ee378543b2e89ce1079c6e6
Rich Tong <1782087+richtong@users.noreply.github.com>
2021-03-26 11:03:33 -0700

    install-powerline.sh switch to powerline-go and vim-airline

--- install-powerline.sh
+++ install-powerline.sh
@@ -13,15 +13,19 @@ set -u && SCRIPTNAME="$(basename "${BASH_SOURCE[0]}")"
 SCRIPT_DIR=${SCRIPT_DIR:=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
 # this replace set -e by running exit on any error use for bashdb
 trap 'exit $?' ERR
+INSTALL_POWERLINE=${INSTALL_POWERLINE:-false}
+VIM_PROFILE="${VIM_PROFILE:-"$HOME/.vimrc"}"
 OPTIND=1
 export FLAGS="${FLAGS:-""}"
-while getopts "hdv" opt; do
+while getopts "hdvp" opt; do
 	case "$opt" in
 	h)
 		cat <<-EOF
 			Installs Powerline
 			    usage: $SCRIPTNAME [ flags ]
 			    flags: -d debug, -v verbose, -h help"
+					   -f install powerline not powerline-go and vim-airline
+					   (default: $INSTALL_POWERLINE)
 		EOF
 		exit 0
 		;;
@@ -32,6 +36,9 @@ while getopts "hdv" opt; do
 		export VERBOSE=true
 		# add the -v which works for many commands
 		export FLAGS+=" -v "
+		;;
+	p)
+		INSTALL_POWERLINE=true
 		;;
 	*)
 		echo "not flag -$opt" >&2
@@ -44,14 +51,63 @@ if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
 
 source_lib lib-install.sh lib-util.sh lib-config.sh
 
+log_verbose "install powerline-go and powerline-status packages"
 pip_install powerline-status powerline-gitstatus
 
+brew_install powerline-go
+
+log_verbose "installing powerline fonts"
+cask_install font-fira-mono-for-powerline
+
+if ! $INSTALL_POWERLINE; then
+	log_verbose "Installing Powerline-Go"
+	# https://github.com/vim-airline/vim-airline
+	# recommend .profile but .bashrc works better
+	# for pipenv shell etc
+	if ! config_mark "$(config_profile_shell)"; then
+		config_add "$(config_profile_shell)" <<'EOF'
+function _update_ps1() {
+    # shellcheck disable=SC2046
+    PS1=$(powerline-go -hostname-only-if-ssh -max-width 30
+		  -colorize-hostname -shorten-gke-names -theme solarized-dark16
+		  -modules venv,user,host,ssh,cwd,perms,git,hg,jobs,exit,root,docker,docker-content,goenv,kube
+		  -error $? -jobs "$(jobs -p | wc -l)")
+}
+if [[ $TERM != linux ]] && command -v powerline-go >& /dev/null; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
+EOF
+	fi
+	# https://github.com/vim-airline/vim-airline
+	log_verbose "Installing vim-airline this needs to go in PlugBegin"
+	if ! config_mark "$VIM_PROFILE" \"; then
+		config_add "$VIM_PROFILE" <<-EOF
+			" Add in Plug Begin
+			" Plug 'vim-airline/vim-airline'
+			" Plug 'vim-airline/vim-airline-themes'
+			let g:airline#extensions#tabline#enabled = 1
+		EOF
+	fi
+	log_exit "success installing vim-airline and powerline-go"
+fi
+
+log_warning "powerline-status doe not work with python 3.9"
+log_verbose "Installing Powerline and Vim addon"
+
+log_verbose "installing powerline control scripts"
 location="$(pip show powerline-status | grep Location | awk '{print $2}')"/powerline
 log_verbose "powerline completion script at $location"
 
 powerline="$location/bindings/bash/powerline.sh"
 if [[ ! -e $powerline ]]; then
 	log_error 2 "cannot find powerline at $powerline"
+fi
+
+config="$location/config_files"
+PROFILE="${PROFILE:-"$HOME/.config/powerline"}"
+if [[ ! -e $PROFILE/config.json ]]; then
+	log_verbose "copying from $config to $PROFILE"
+	cp -r "$config/"* "$PROFILE"
 fi
 
 if ! config_mark "$(config_profile_shell)"; then
@@ -65,25 +121,13 @@ fi
 EOF
 fi
 
-config="$location/config_files"
-PROFILE="${PROFILE:-"$HOME/.config/powerline"}"
-if [[ ! -e $PROFILE/config.json ]]; then
-	log_verbose "copying from $config to $PROFILE"
-	cp -r "$config/"* "$PROFILE"
-fi
-
-log_verbose "installing powerline fonts add to config.json"
-cask_install font-fira-mono-for-powerline
-
-VIM_PROFILE="${VIM_PROFILE:-"$HOME/.vimrc"}"
-if ! config_mark "$VIM_PROFILE" '"'; then
+if ! config_mark "$VIM_PROFILE" \"; then
 	log_verbose "adding to $VIM_PROFILE"
-	config_add "$VIM_PROFILE" <<-EOF
+	config_add "$VIM_PROFILE" '"' <<-EOF
 		set rtp+="$location/bindings/vim"
 		set laststatus=2
 	EOF
 fi
-
 # https://github.com/gravyboat/powerline-config
 
 log_verbose "change config at $PROFILE"
