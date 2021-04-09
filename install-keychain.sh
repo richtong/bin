75d326e4cc6fac8525bac88b91a60fd031a07f93
Rich Tong <1782087+richtong@users.noreply.github.com>
2021-04-08 19:21:01 -0700

    install-keychain for linux WFS works

--- install-keychain.sh
+++ install-keychain.sh
@@ -7,14 +7,16 @@
 #
 set -e && SCRIPTNAME=$(basename "${BASH_SOURCE[0]}")
 SCRIPT_DIR=${SCRIPT_DIR:-"$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"}
-
 OPTIND=1
-while getopts "hdv" opt; do
+SSH_DIR="${SSH_DIR:-"$HOME/.ssh"}"
+while getopts "hdvk:" opt; do
 	case "$opt" in
 	h)
-		echo "$SCRIPTNAME: Install correct keychain and list of keys"
-		echo "flags: -d debug, -v verbose, -h help"
-		echo "positional: list of keys"
+		cat <<-EOF
+		$SCRIPTNAME: Install correct keychain and list of keys
+		flags: -d debug, -v verbose, -h help
+			   directory with keys to be added (default: all keys in $SSH_DIR)
+		EOF
 		exit 0
 		;;
 	d)
@@ -22,6 +24,9 @@ while getopts "hdv" opt; do
 		;;
 	v)
 		export VERBOSE=true
+		;;
+	k)
+		SSH_DIR="$OPTARG"
 		;;
 	*)
 		echo "no -$opt" >&2
@@ -31,23 +36,38 @@ done
 
 # shellcheck source=./include.sh
 if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
-source_lib lib-keychain.sh
-
-ORG_DOMAIN="${ORG_DOMAIN:-tongfamily.com}"
-
+source_lib lib-keychain.sh lib-install.sh lib-util.sh lib-config.sh
 set -u
 shift $((OPTIND - 1))
 
-if (($# == 0)); then
-	KEYS="$HOME/.ssh/$USER@$ORG_DOMAIN-github.com.id_ed25519 "
-else
-	# https://stackoverflow.com/questions/255898/how-to-iterate-over-arguments-in-a-bash-script
-	KEYS="$*"
+if ! in_os linux; then
+	log_verbose "linux only"
 fi
 
-log_verbose "using keys $KEYS"
+package_install keychain
+
+if (($# > 1)); then
+	# https://stackoverflow.com/questions/255898/how-to-iterate-over-arguments-in-a-bash-script
+	KEYS=("$@")
+else
+	log_verbose "look in $SSH_DIR for keys"
+	# https://stackoverflow.com/questions/23356779/how-can-i-store-the-find-command-results-as-an-array-in-bash/54561526#54561526
+	# these may be symlinks so need -L
+	mapfile -d "" KEYS < <(find -L "$SSH_DIR" -name "*.id_ed25519" -o -name "*.id_rsa")
+	log_verbose "found ${KEYS[*]}"
+fi
+
+if ! config_mark; then
+	config_add <<-EOF
+		keychain "${KEYS[@]}"
+		source "$HOME/.keychain/$(uname -n)-sh"
+	EOF
+fi
+source_profile
+
+log_verbose "using keys ${KEYS[*]}"
 log_verbose "ssh agent is $(env | grep SSH)"
 # shellcheck disable=SC2086
-if ! use_openssh_keychain $KEYS; then
+if ! use_openssh_keychain "${KEYS[@]}"; then
 	echo "reboot needed then rerun"
 fi
