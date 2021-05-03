efd4f5e0f7988363049917ad9ac8a9f080e5d721
Rich Tong <rich@tongfamily.com>
2021-05-02 19:00:29 -0700

    fixes with multipass but kubeflow needs sock proxy

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -11,9 +11,10 @@ SCRIPT_DIR=${SCRIPT_DIR:-"$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"}
 DOCKER="${DOCKER:-"false"}"
 MINIKUBE="${MINIKUBE:-"false"}"
 FORCE="${FORCE:-"false"}"
-MICROK8S="${MICROK8S:-"true"}"
+MICROK8S="${MICROK8S:-"false"}"
+MULTIPASS_ONLY="${MULTIPASS_ONLY:-true}"
 OPTIND=1
-while getopts "hdvmuof" opt; do
+while getopts "hdvmuiof" opt; do
 	case "$opt" in
 	h)
 		cat <<-EOF
@@ -21,7 +22,8 @@ while getopts "hdvmuof" opt; do
 			flags: -d debug, -v verbose, -h help
 				-f force installation (default: $FORCE)
 				-m minikube (default: $MINIKUBE)
-				-u MicroK8s current hangs on MacOS (default: $MICROK8S)
+				-i MicroK8s using Multipass does not work with kubeflow (default: $MICROK8S)
+				-u Multipass with Microk8s available only inside VM (default: $MULTIPASS_ONLY)
 				-o Docker has a single cluster version (default: $DOCKER)
 		EOF
 		exit 0
@@ -38,8 +40,11 @@ while getopts "hdvmuof" opt; do
 	m)
 		MINIKUBE=true
 		;;
-	u)
+	i)
 		MICROK8S=true
+		;;
+	u)
+		MULTIPASS_ONLY=false
 		;;
 	o)
 		DOCKER=true
@@ -116,6 +121,7 @@ if in_os mac; then
 		if ! microk8s --help >/dev/null; then
 			# https://github.com/canonical-web-and-design/microk8s.io/issues/239
 			log_verbose "microk8s failed delete vm and retry"
+			microk8s un9install
 			multipass delete microk8s-vm
 			multipass purge
 			if ! microk8s install; then
@@ -159,13 +165,67 @@ if in_os mac; then
 			log_error 3 "sudo chown failed"
 		fi
 
+		log_verbose "restart microk8s-vm"
+		if ! multipass restart microk8s-vm; then
+			log_error 5 "could not restart microk8s"
+		fi
+
 		log_verbose "running kubeflow"
 		if ! multipass exec microk8s-vm -- microk8s enable kubeflow --ignore-min-mem --bundle lite; then
 			log_error 4 "enable kubeflow failed"
 		fi
 
+		# https://ubuntu.com/tutorials/install-microk8s-on-mac-os?_ga=2.190792939.1724531887.1619986586-1495806297.1619986586#6-deploy-an-app
+		log_verbose "Running demonstration apps at kubernetes bootcamp"
+		if $VERBOSE; then
+			microk8s kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1
+		fi
+		log_verbose "wait a few minutes then run microk8s get pods and microk8s stop when done"
 	fi
+
+	# https://www.techrepublic.com/article/how-to-quickly-spin-up-microk8s-with-multipass/
+	if $MICROK8S_ONLY; then
+		log_verbose "do not use microk8s but install directly into multipass"
+		package_install multipass
+
+		if multipass info microk8s-vm; then
+			multipass stop microk8s-vm
+			multipass delete microk8s-vm
+			multipass purge
+		fi
+
+		multipass launch --name microk8s-vm --mem 3G
+		multipass exec microk8s-vm -- sudo snap install microk8s --classic
+		multipass exec microk8s-vm -- sudo iptables -P FORWARD ACCEPT
+		# note the shell escaping does not work properly without bash -c
+		# shellcheck disable=SC2016
+		multipass exec microk8s-vm -- bash -c 'sudo usermod -aG microk8s $USER'
+		# shellcheck disable=SC2016
+		multipass exec microk8s-vm -- bash -c 'sudo chown -f -R $USER $HOME/.kube'
+
+		if $VERBOSE; then
+			# https://www.kubeflow.org/docs/distributions/microk8s/kubeflow-on-microk8s/
+			log_verbose "test with nginx"
+			multipass exec microk8s-vm -- microk8s kubectl run nginx --image=nginx:alpine --replicas=2 --port=80
+			multipass exec microk8s-vm -- microk8s kubectl get pod
+			log_verbose "enable kubeflow ignoring 8GB minimum memory"
+			multipass exec microk8s-vm -- microk8s enable kubeflow --ignore-min-mem
+			multipass list | grep microk8s-vm
+			# https://stackoverflow.com/questions/18592173/select-objects-based-on-value-of-variable-in-object-using-jq
+			ip=$(multipass list --format=json | jq '.list[] | select(.name=="microk8s-vm")| .ipv4[0]')
+			# https://richardkmiller.com/925/script-to-enabledisable-socks-proxy-on-mac-os-x
+			networksetup -setsocksfirewallproxy Wi-Fi 127.0.0.1 9999
+			networksetup -setsocksfirewallproxy Ethernet 127.0.0.1 9999
+			networksetup -setsocksfirewallproxystate Wi-Fi on
+			networksetup -setsocksfirewallproxystate Ethernet on
+			log_warning "point the socks proxy to the multipass vm"
+			log_verbose "ssh -D9999 multipass@$ip"
+		fi
+
+	fi
+
 else
+
 	log_verbose "curl from $KUBE_URL"
 	sudo curl -L "$KUBE_URL" -o "$KUBE_DEST"
 	sudo chmod +x "$KUBE_DEST"
