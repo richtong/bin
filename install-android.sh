2be7e8ed8f5d514d614705e4a6893335e2267050
Rich Tong <1782087+richtong@users.noreply.github.com>
2022-07-27 15:59:35 -0700

    feat: android studio
    Installation of the Android studio and devleopment kit

---
+++ install-android.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+## vim: set noet ts=4 sw=4:
+##
+## install 1Password
+## https://news.ycombinator.com/item?id=9091691 for linux gui
+## https://news.ycombinator.com/item?id=8441388 for cli
+## https://www.npmjs.com/package/onepass-cli for npm package
+## ##@author Rich Tong
+##@returns 0 on success
+#
+set -ueo pipefail && SCRIPTNAME="$(basename "${BASH_SOURCE[0]}")"
+SCRIPT_DIR=${SCRIPT_DIR:=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
+DEBUGGING="${DEBUGGING:-false}"
+VERBOSE="${VERBOSE:-false}"
+VERSION="${VERSION:-7}"
+EMAIL="${EMAIL:-rich@tongfamily.com}"
+SIGNIN="${SIGNIN:-my}"
+OPTIND=1
+export FLAGS="${FLAGS:-""}"
+
+while getopts "hdv" opt; do
+	case "$opt" in
+	h)
+		cat <<-EOF
+			Installs 1Password
+			usage: $SCRIPTNAME [ flags ]
+			flags:
+				   -h help
+				   -d $(! $DEBUGGING || echo "no ")debugging
+				   -v $(! $VERBOSE || echo "not ")verbose
+				   -r version number (default: $VERSION)
+				   -e email for login (default: $EMAIL)
+			                   -s signin subdomain add to .1password.com (default: $SIGNIN)
+		EOF
+		exit 0
+		;;
+	d)
+		# invert the variable when flag is set
+		DEBUGGING="$($DEBUGGING && echo false || echo true)"
+		export DEBUGGING
+		;;
+	v)
+		VERBOSE="$($VERBOSE && echo false || echo true)"
+		export VERBOSE
+		# add the -v which works for many commands
+		if $VERBOSE; then export FLAGS+=" -v "; fi
+		;;
+	*)
+		echo "no flag -$opt"
+		;;
+	esac
+done
+shift $((OPTIND - 1))
+# shellcheck source=./include.sh
+if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
+
+source_lib lib-git.sh lib-mac.sh lib-install.sh lib-util.sh
+
+cask_install android-studio
