1155d1cfd083e0231a492a15d647b2caa3bc28ee
Rich <rich@tongfamily.com>
2022-12-28 21:55:35 +0000

    fix: install-rdp for ubuntu
    Fixes for ubuntu instlalation and the need for CoreKeyboard

--- install-rdp.sh
+++ install-rdp.sh
@@ -54,7 +54,12 @@ if in_os mac; then
 	mas_install 1295203466
 
 elif in_os linux; then
+
+	if sudo systemctl status xrdp >/dev/null; then
+		log_verbose "RDP already installed"
+	fi
 	# https://linuxize.com/post/how-to-install-xrdp-on-ubuntu-20-04/
+	log_verbose "No RDP install xrdp"
 	package_install xrdp
 	log_verbose "Add xrdp to ssl-cert group"
 	sudo adduser xrdp ssl-cert
@@ -62,4 +67,12 @@ elif in_os linux; then
 	log_verbose "if ufw is active enable port 3389"
 	sudo systemctl restart xrdp
 
+	# https://ubuntututorials.org/ubuntu-20-04-xrdp-black-screen/
+	log_warning "You cannot be logged into to the local interface while trying to RDP in"
+	# https://github.com/neutrinolabs/xorgxrdp/issues/164
+	log_verbose "Ubuntu 20.04 adding Option CoreKeyboard and Option CorePointer"
+	grep -q 'Option "CoreKeyboard"' /etc/X11/xrdp/xorg.conf ||
+		sudo sed -i '/^[[:blank:]]*Driver "xrdpkeyb"/a\    Option "CoreKeyboard"' /etc/X11/xrdp/xorg.conf
+	grep -q 'Option "CorePointer"' /etc/X11/xrdp/xorg.conf ||
+		sudo sed -i '/^[[:blank:]]*Driver "xrdpmouse"/a\    Option "CorePointer"' /etc/X11/xrdp/xorg.conf
 fi
