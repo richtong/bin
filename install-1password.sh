89d224dc75e55825cbb6f171ee0c68566e85835c
Rich Tong <1782087+richtong@users.noreply.github.com>
2024-09-13 11:31:00 -0700

    feat: auth0install
    this is now a separate install

--- install-1password.sh
+++ install-1password.sh
@@ -195,10 +195,6 @@ elif in_os mac; then
 
 fi
 
-log_verbose "Installing auth0 cli"
-tap_install auth0/auth0-cii
-brew install auth0
-
 PACKAGE+=(
 	1password-cli
 )
---
+++ install-auth0.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+# vim: set noet ts=4 sw=4:
+#
+## install auth0 cli
+## @author Rich Tong
+## @returns 0 on success
+#
+# https://chezmoi.io/
+#
+set -ueo pipefail && SCRIPTNAME="$(basename "${BASH_SOURCE[0]}")"
+SCRIPT_DIR=${SCRIPT_DIR:=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
+DEBUGGING="${DEBUGGING:-false}"
+VERBOSE="${VERBOSE:-false}"
+FORCE="${FORCE:-false}"
+export FLAGS="${FLAGS:-""}"
+
+OPTIND=1
+while getopts "hdvf" opt; do
+	case "$opt" in
+	h)
+		cat <<-EOF
+			Installs Auth0 CLI
+			usage: $SCRIPTNAME [ flags ]
+			flags:
+				   -h help
+				   -d $($DEBUGGING && echo "no ")debugging
+				   -v $($VERBOSE && echo "not ")verbose
+				   -f $($FORCE && echo "do not ")force install even $SCRIPTNAME exists
+
+		EOF
+		exit 0
+		;;
+	d)
+		# invert the variable when flag is set
+		DEBUGGING="$($DEBUGGING && echo false || echo true)"
+		export DEBUGGING
+		;;
+	v)
+		VERBOSE="$($VERBOSE && echo false || echo true)"
+		export VERBOSE
+		# add the -v which works for many commands
+		if $VERBOSE; then export FLAGS+=" -v "; fi
+		;;
+	f)
+		FORCE="$($FORCE && echo false || echo true)"
+		export FORCE
+		;;
+	*)
+		echo "no flag -$opt"
+		;;
+	esac
+done
+shift $((OPTIND - 1))
+# shellcheck disable=SC1091
+if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
+source_lib lib-git.sh lib-mac.sh lib-install.sh lib-util.sh lib-config.sh
+
+log_verbose "Installing auth0 cli"
+tap_install auth0/auth0-cii
+PACKAGE+=(
+	auth0
+)
+
+log_verbose "Install ${PACKAGE[*]}"
+package_install "${PACKAGE[@]}"
