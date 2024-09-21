667d7a6470a8f97414ededce777e9bcbf07c45ff
Rich Tong <1782087+richtong@users.noreply.github.com>
2024-09-21 15:34:20 -0700

    feat: adding ai and ollama
    This now works correctly for ollama and openwebui and enchanted

--- install-ollama.sh
+++ install-ollama.sh
@@ -19,7 +19,7 @@ while getopts "hdvr:e:s:" opt; do
 	case "$opt" in
 	h)
 		cat <<-EOF
-			Installs AI Tools including Stability Diffusion and ChatGPT
+			Installs Ollama and models
 			usage: $SCRIPTNAME [ flags ]
 			flags:
 				   -h help
@@ -51,6 +51,7 @@ if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
 
 source_lib lib-git.sh lib-mac.sh lib-install.sh lib-util.sh lib-config.sh
 
+#
 # https://lobehub.com/blog/5-ollama-web-ui-recommendation
 
 PACKAGE+=(
@@ -61,22 +62,22 @@ PACKAGE+=(
 	# poe - a chatbot aggregator by Quora, allows multiple chats (not using)
 	# shell-gpt - cli including running shell commands (never use deprecated)
 	# vincelwt-chatgpt - ChatGPT in menubar (not using)
-	appflowy
-	diffusionbee    # diffusionbee - Stability diffusion on Mac
-	jan             # yet another ollama close
-	lm-studio       # lm-studio -  run different LLMs from Hugging Face locally
-	mochi-diffusion # mochi-diffusion - Stability diffusion on Mac (hanvent' used
+	ollama  # ollama - ollama local runner
+	ollamac # ollamac is a self contained mac app crashes on startup
 
 )
+package_install "${PACKAGE[@]}"
 
 MAS+=(
 	6474268307 # Enchanted LLM Mac only selfhosted
+
 )
 mas_install "${MAS[@]}"
 
 PIP_PACKAGE+=(
 
-	open-interpreter
+	open-interpreter # local llm run with interpreter at the cli with interpreter
+	open-webui       # create a web app at localhost:8080 run with open-webui server
 
 )
 
@@ -88,10 +89,47 @@ PIP_PACKAGE+=(
 #ARCH=arm64
 #fi
 #download_url_open "https://github.com/vincelwt/chatgpt-mac/releases/download/v0.0.5/ChatGPT-0.0.5-$ARCH.dmg"
-package_install "${PACKAGE[@]}"
 log_verbose "Pip install only in current environment rerun in other venvs"
 pip_install --upgrade "${PIP_PACKAGE[@]}"
 log_warning "shell-gpt requires OPENAI_API_KEY to be set or will store in ~/.config/shell_gpt/.sgptrc"
 
 # no need for gp4all
 #download_url_open "https://gpt4all.io/installers/gpt4all-installer-darwin.dmg"
+
+if ! config_mark "$(config_profile_interactive)"; then
+	config_add "$(config_profile_interactive)" <<-EOF
+		if command -v open-webui > /dev/null; then
+			open-webui --install-completion
+		fi
+	EOF
+fi
+
+if (($(pfind ollama | wc -l) == 0)); then
+	log_verbose "ollama serve starting"
+	ollama serve &
+fi
+
+MODEL+=(
+
+	llama3.1                   # 8B, 98B-instruct-q4_0
+	llama3.1:8b-text-q4_0      # test tuneded
+	llama3.1:70b-instruct-q4_0 # 70b instruct 4bit- quantized
+	gemma2                     # Google
+	qwen2.5                    # Alibaba 2024-09-16
+	qwen2.5-coder              # code specific
+	phi3.5                     # Microsoft 3.8B-instruct-q4_0
+	nemotron-mini              # nViida ropepaly, TAG QSA and function calling
+	starcoder2                 ## 3B and 7B
+	bespoke-minicheck          # Fact check 7B
+	reader-lm                  # HTML to Markdown conversion 0.5B and 1.5B
+	deepseek-v2.5              # 2024-09-03 integrated chat and instruct
+	yi-coder                   # Coding 9B-instruct-q4_0
+	mistral-large              # 128K context winod 123B
+
+)
+
+log_verbose "Pulling ${MODEL[*]}"
+for M in "${MODEL[@]}"; do
+	log_verbose "Pulling model $M"
+	ollama pull "$M"
+done
