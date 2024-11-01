5c298b03987a95be3430fd00fe8a9c2a3656df56
Rich Tong <1782087+richtong@users.noreply.github.com>
2024-10-31 17:50:51 -0700

    fix: adding ibm granite and other models for 10-31-24
    ADding new models and resort based on time. Also add the size parameter
    explicitly.

--- install-ollama.sh
+++ install-ollama.sh
@@ -51,7 +51,6 @@ if [[ -e "$SCRIPT_DIR/include.sh" ]]; then source "$SCRIPT_DIR/include.sh"; fi
 
 source_lib lib-git.sh lib-mac.sh lib-install.sh lib-util.sh lib-config.sh
 
-#
 # https://lobehub.com/blog/5-ollama-web-ui-recommendation
 
 PACKAGE+=(
@@ -79,8 +78,7 @@ mas_install "${MAS[@]}"
 
 PYTHON_PACKAGE+=(
 
-	open-interpreter # local llm run with interpreter at the cli with interpreter
-	open-webui       # create a web app at localhost:8080 run with open-webui server
+	open-webui # create a web app at localhost:8080 run with open-webui server
 
 )
 
@@ -125,58 +123,66 @@ fi
 # These are too large for a 64GB machine
 # deepseek-v2.5              # 2024-09-03 instruct 236B very large
 MODEL+=(
-	# coder models
-	# deepseek-coder-v2 # 16-lite-base-q4_0 -- deepseek 2.5 replaces
-	codestral      # 22B Mistral code model 32K context window
-	qwen2.5-coder  # 128K Tuned for coding 7B
-	starcoder2     ## 3B chat 16K context
-	starcoder2:7b  # 4GB
-	starcoder2:15b # 9GB
-	yi-coder       # 9B model q4 128K context
+	# now sorted by date added from ollama
+	aya-expanse:8b       # cohere model 128k content 23 languages
+	aya-expanse:32b      # cohere model 128k content
+	granite3-guardian:2b #  prompt guard ibm
+	granite3-guardian:8b #  prompt guard ibm
+	granite3-moe:1b      # mixture of experts low latency
+	granite3-moe:3b
+	granite3-dense:2b    # RAG, code  generation, translation
+	granite3-dense:8b    # RAG, code  generation, translation
+	nemotron:70b         # nviida tuned llama 3.1
+	shieldgemma:2b       # safety of text prompts
+	shieldgemma:9b       # safety of text prompts
+	shieldgemma:27b      # safety of text prompts
+	llama-guard3:1b      # safety of prompts
+	llama-guard3:8b      # safety of prompts
+	qwen2.5-coder:1.5b   # 128K Tuned for coding 7B
+	qwen2.5-coder:7b     # 128K Tuned for coding 7B
+	llama3.2:3b          # Meta 3.2-3B Q4 128 context 2GB
+	llama3.2:1b          # Meta 1B 128K context
+	qwen2.5:7b           # 128K context Alibaba 2024-09-16 7b
+	qwen2.5:14b          # 128K context Alibaba 2024-09-16 7b
+	qwen2.5:32b          # 128K context Alibaba 2024-09-16 7b
+	qwen2.5:72b          # 128K context Alibaba 2024-09-16 7b
+	solar-pro:22b        # 22b comparable to llama 3.1 70b 4k context
+	nemotron-mini:4b     # nVidia ropeplay, Q&A and function calling 4b-instruct-q4_K-M
+	bespoke-minicheck:7b # Fact check 7B q4_K_M
+	mistral-small:22b    # v0.3 Mistral 22b-instruct-2409-q4_0
+	reader-lm:0.5b       # HTML to Markdown conversion 1.5B-q4_K_M
+	reader-lm:1.5b       # HTML to Markdown conversion 1.5B-q4_K_M
+	yi-coder:9b          # 9B model q4 128K context
+	minicpm-v:8b         # vision mLLM 8b-2.6-q4_0
 
-	# general purpose
+	# these models are post llama3.1
 	# llama3.1:8b-text-q4_0 # text tuned model poor
-	# qwen2-math              # Alibaba math 7b-instruct-q4_K_M
-	command-r                 # 128K context 35b 19GB
-	gemma2                    # Google 9B Q4 5.4GB 8K context
-	gemma2:27b                # Q4 16GB 8K context
-	hermes3                   # fine tuned llama 3.1 8B 128K context
-	hermes3:70b               # fine tuned llama 3.1 q4 128K Context
-	llama3.2                  # Meta 3.2-3B Q4 128 context 2GB
-	llama3.2:3b-instruct-q8_0 # 3GB
-	mistral-nemo              # 128k context 12b-instruct-2407-q4_0
-	mistral-small             # v0.3 Mistral 22b-instruct-2409-q4_0
-	qwen2.5                   # 128K context Alibaba 2024-09-16 7b
-	solar-pro                 # 22b comparable to llama 3.1 70b 4k context
 
-	# speciality models
-	bespoke-minicheck                                                    # Fact check 7B q4_K_M
-	reader-lm                                                            # HTML to Markdown conversion 1.5B-q4_K_M
-	shieldgemma                                                          # safety of text prompts
-	llama-guard3                                                         # safety of prompts
-	bge-large                                                            # tokens to embeddings
-	firefunction-v2                                                      # gpt-40 level function calling
+	starcoder2:7b         # 4GB
+	starcoder2:15b        # 9GB
+	deepseek-coder-v2:16b # 16-lite-base-q4_0 -- deepseek 2.5 replaces but no 16b yet
+	phi3.5:3.8b           # Microsoft 3.8B-instruct-q4_0 beaten by llama3.2?
+	codestral:22b         # 22B Mistral code model 32K context window
+	# hermes3                   # fine tuned llama 3.1 8B 128K context
+	# hermes3:70b               # fine tuned llama 3.1 q4 128K Context
+	command-r:35b    # 128K context 35b 19GB
+	bge-large:335m   # tokens to embeddings
+	mistral-nemo:12b # 128k context 12b-instruct-2407-q4_0
+	gemma2:9b        # Google 9B Q4 5.4GB 8K context
+	gemma2:27b       # Q4 16GB 8K context
+	mistral:7b       # 4GB 7B Q4
+	mixtral:8x7b     # mistral moe
+	moondream2:1.8b  # small vision LLM
+	llava-llama3     # older vision model based on llama3
+
 	hf.co/ICEPVP8977/Uncensored_gemma_2b                                 # make sure your hf enabled for shield tests
 	hf.co/mradermacher/Llama-3.2-3B-Instruct-Spatial-SQL-1.0-GGUF:Q4_K_S # sql model
 	hf.co/PurpleAILAB/SQL_Llama-3.2-3B-Instruct-uncensored_final-gguf    # living dangerously
 
-	# small LLMs
-	# nemotron-mini # nVidia ropeplay, Q&A and function calling 4b-instruct-q4_K-M
-	gemma2:2b # Q4 1.6GB 8K context RoPE
-	phi3.5    # Microsoft 3.8B-instruct-q4_0 beaten by llama3.2?
-
 	# purpose large models
 	# llama3.1              # deprecated
 	# mistral-large:123b-instruct-2407-q3_K_S # 128K context winod 123B-instruct-q3_K_S
-	command-r-plus:104b-q2_K              # 39GB Q2 with 128K context for enterprise
-	llama3.1:70b                          # 70b-instruct-q4_0  40GB 128K context
-	qwen2.5:72b                           # 128K context Alibaba 72b-instruct-q4_K-M instruct
-	mistral-large:123b-instruct-2407-q2_K # Mistral v2 128k context only q2_K will fit
-
-	# vision
-	minicpm-v    # vision mLLM 8b-2.6-q4_0
-	llava-llama3 # older vision model based on llama3
-	llava        # older vision model
+	# command-r-plus:104b-q2_K # 39GB Q2 with 128K context for enterprise
 
 )
 
